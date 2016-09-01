2cc3d3b512ad2c615f0286ffa600cfc26a098d2a
Anthony Vanrenterghem <a1.vanrenterghem@univ-lille1.fr>
2016-09-01 11:02:33 +0200

    Bombe

---
+++ src/Bombe.java
@@ -0,0 +1,18 @@
+public class Bombe extends Item{
+	
+	int[] coords;
+	int portee=1;
+	char name='B';
+	
+	public void setCoords(int[]coords){
+		this.coords=coords;
+	}
+	
+	public int[] getCoords(){
+		return this.coords;
+	}
+
+	String Action() {
+		
+	}
+}
