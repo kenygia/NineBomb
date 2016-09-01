af3a6f1bbba58f61ce6f18a3ca3031169f3bd3b8
Keenen Gilles Harrisson <kenydbzgt@gmail.com>
2016-09-01 11:33:55 +0200

    Plateau maj

--- src/Menu.java
+++ src/Menu.java
@@ -3,38 +3,48 @@ public class Menu
 {
 
 	Personnage p;
+	Plateau m;
 	
-	public Menu(Personnage perso)
+	public Menu(Personnage perso, Plateau map)
 	{
 		this.p = perso;
-		
+		this.m = map;
 	}
 
 	// deplacement 
 	
 	public void deplaceNORD()
 	{
-		
+		int [] mod = new int[] {0, -1};
+		if (this.m.checkPerso(this.p, mod))
+		{
+			m.deplace(p.coords, mod);
+			p.setCoords(mod);
+		}
 	}
 	
 	public void deplaceEST()
 	{
+		int [] coords = new int[] {1, 0};
 		
 	}
 	
 	public void deplaceSUD()
 	{
+		int [] coords = new int[] {0, 1};
 		
 	}
 	
 	public void deplaceOUEST()
 	{
+		int [] coords = new int[] {-1, 0};
 		
 	}
 	
-	public void poserBomob()
+	public void poserBomb()
 	{
 		
 	}
+	
 	
 }
--- src/Plateau.java
+++ src/Plateau.java
@@ -1,35 +1,61 @@
+import sort.Item;
 
 public class Plateau {
 
-	private char [][] theMap;
+	private Case [][] theMap;
 	private int longueur;
 	private int largeur;
 	private int haut;
+	private char[][] map;
+	private Case vide;
 	
 	public Plateau (int longueur, int largeur){
-		this.theMap = new char [longueur][largeur];
+		this.theMap = new Case [longueur][largeur];
 		this.longueur = longueur;
 		this.largeur = largeur;
 		
-		this.theMap = new char[][]{{' ',' ','M',' ','P'},
+		vide = new Case(null, null, 'V', null);
+		//this.theMap
+		this.map = new char[][]{{' ',' ','M',' ','P'},
 									{' ','D','M','D',' '},
 									{'D','M','D','D','M'}};
 		
 	}
 	
-	public char retourVal (int x, int y){
-		return this.theMap[x][y];
+	public void deplace(int[] coords, int[] mod)
+	{
+
+		int x = coords[0];
+		int y = coords[1];
+		int nx = coords[0]+mod[0];
+		int ny = coords[1]+mod[1];
+		
+		this.theMap[nx][ny] = this.theMap[x][y];
+		this.theMap[x][y] = this.vide;
 	}
 	
-	public int[][] size(char [][] tab){
+	public boolean checkPerso(Personnage p, int[] mod)
+	{
+		int x = p.getCoords()[0]+mod[0];
+		int y = p.getCoords()[1]+mod[1];
+		if ((largeur > y) && (longueur > x) && (x >= 0) && (y >= 0))
+			return !(this.theMap[x][y].Blocage());
+		return false;
+	}
+	
+	public char retourVal (int x, int y){
+		return this.theMap[x][y].Priorite();
+	}
+	
+	public int[][] size(){
 		return new int [this.longueur][this.largeur];
 	}
 	
 	public String toString(){
 		String chaine = "";
-		for(int i=0;i<this.theMap.length;i++){
-			for(int j=0;j<this.theMap[0].length;j++){
-				chaine += this.theMap[i][j];
+		for(int i=0;i<this.map.length;i++){
+			for(int j=0;j<this.map[0].length;j++){
+				chaine += this.map[i][j]/*.Priorite()*/;
 			}
 			chaine += "\n";
 		}
