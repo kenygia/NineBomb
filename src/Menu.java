42df89692619019f49bea9fd3db3dd21bf24c924
Keenen Gilles Harrisson <kenydbzgt@gmail.com>
2016-09-01 09:50:06 +0200

    Menu protos sans fonction

---
+++ src/Menu.java
@@ -0,0 +1,40 @@
+
+public class Menu
+{
+
+	Personnage p;
+	
+	public Menu(Personnage perso)
+	{
+		this.p = perso;
+		
+	}
+
+	// deplacement 
+	
+	public void deplaceNORD()
+	{
+		
+	}
+	
+	public void deplaceEST()
+	{
+		
+	}
+	
+	public void deplaceSUD()
+	{
+		
+	}
+	
+	public void deplaceOUEST()
+	{
+		
+	}
+	
+	public void poserBomob()
+	{
+		
+	}
+	
+}
