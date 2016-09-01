daabc2ea0a3ac4481b9682387473936e0b47d6d7
Anthony Vanrenterghem <a1.vanrenterghem@univ-lille1.fr>
2016-09-01 08:34:42 +0200

    Personnage

---
+++ src/Personnage.java
@@ -0,0 +1,13 @@
+
+public class Personnage {
+	char[] touche = new char[]{'z','q','s','d',' '};
+	String nom ;
+	
+	public Personnage(){
+		nom="Player 1";
+	}
+	
+	public Personnage(String chaine){
+		nom=chaine;
+	}
+}
