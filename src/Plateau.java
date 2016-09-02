b2d70d2dfb55e4c5bdf6a03fede809d76547e88d
AlexandrePellerin <pellerin.alex@orange.fr>
2016-09-02 08:15:36 +0200

    Plateau

--- src/Plateau.java
+++ src/Plateau.java
@@ -1,8 +1,8 @@
-import sort.Item;
+import java.util.ArrayList;
 
 public class Plateau {
 
-	private Case [][] theMap;
+	private Case[][] theMap;
 	private int longueur;
 	private int largeur;
 	private int haut;
@@ -10,25 +10,25 @@ public class Plateau {
 	private Case vide;
 	private Case murD;
 	private Case murI;
-	
-	public Plateau (int longueur, int largeur){
-		this.theMap = new Case [longueur][largeur];
+	private ArrayList<Bombe> bombes;
+
+	public Plateau(int longueur, int largeur) {
+		this.theMap = new Case[longueur][largeur];
 		this.longueur = longueur;
 		this.largeur = largeur;
-
+		this.bombes = new ArrayList<>();
+		
 		vide = new Case(null, null, 'V', null);
 		murD = new Case(null, null, 'D', null);
 		murI = new Case(null, null, 'I', null);
-		//this.theMap
-		this.map = new char[][]{{' ',' ','M',' ','P'},
-									{' ','D','M','D',' '},
-									{'D','M','D','D','M'}};							
-		
+		// this.theMap
+		this.map = new char[][] { { ' ', ' ', 'M', ' ', 'P' }, { ' ', 'D', 'M', 'D', ' ' },
+				{ 'D', 'M', 'D', 'D', 'M' } };
+
 	}
-	
-	public Plateau(Personnage one, Personnage two)
-	{
-		this.theMap = new Case [10][10];
+
+	public Plateau(Personnage one, Personnage two) {
+		this.theMap = new Case[10][10];
 		this.longueur = 10;
 		this.largeur = 10;
 
@@ -37,61 +37,76 @@ public class Plateau {
 		murI = new Case(null, null, 'I', null);
 		Case o = new Case(one, null, '1', null);
 		Case t = new Case(two, null, '2', null);
-		
-		this.theMap = new Case[][]{{murI,murI,murI,murI,murI,murI,murI,murI,murI,murI}, 
-									{murI,o,vide,murD,murD,murD,murD,murD,vide, murI},
-									{murI,vide,murI,vide,murD,murD,murD,murD,murD, murI},
-									{murI,vide,vide,murI,vide,murI,murI,vide,vide, murI},
-									{murI,murD,murD,murD,murD,murI,murD,murD,murD, murI},
-									{murI,murD,murD,murD,murI,murD,murD,murD,murD, murI},
-									{murI,vide,vide,murI,murI,vide,murI,vide,vide, murI},
-									{murI,murD,murD,murD,murD,murD,vide,murI,vide, murI},
-									{murI,vide,murD,murD,murD,murD,murD,vide,t,murI},
-									{murI,murI,murI,murI,murI,murI,murI,murI,murI,murI}};
+
+		this.theMap = new Case[][] { { murI, murI, murI, murI, murI, murI, murI, murI, murI, murI },
+				{ murI, o, vide, murD, murD, murD, murD, murD, vide, murI },
+				{ murI, vide, murI, vide, murD, murD, murD, murD, murD, murI },
+				{ murI, vide, vide, murI, vide, murI, murI, vide, vide, murI },
+				{ murI, murD, murD, murD, murD, murI, murD, murD, murD, murI },
+				{ murI, murD, murD, murD, murI, murD, murD, murD, murD, murI },
+				{ murI, vide, vide, murI, murI, vide, murI, vide, vide, murI },
+				{ murI, murD, murD, murD, murD, murD, vide, murI, vide, murI },
+				{ murI, vide, murD, murD, murD, murD, murD, vide, t, murI },
+				{ murI, murI, murI, murI, murI, murI, murI, murI, murI, murI } };
 	}
-	
-	public int[] deplace(int[] coords, int[] mod)
-	{
+
+	public int[] deplace(int[] coords, int[] mod) {
 
 		int x = coords[0];
 		int y = coords[1];
-		int nx = coords[0]+mod[0];
-		int ny = coords[1]+mod[1];
-		
+		int nx = coords[0] + mod[0];
+		int ny = coords[1] + mod[1];
+
 		this.theMap[nx][ny] = this.theMap[x][y];
 		this.theMap[x][y] = this.vide;
-		
-		return new int[] {nx,ny};
+
+		return new int[] { nx, ny };
 	}
-	
-	public boolean checkPerso(Personnage p, int[] mod)
-	{
-		int x = p.getCoords()[0]+mod[0];
-		int y = p.getCoords()[1]+mod[1];
+
+	public boolean checkPerso(Personnage p, int[] mod) {
+		int x = p.getCoords()[0] + mod[0];
+		int y = p.getCoords()[1] + mod[1];
 		return !(this.theMap[x][y].Blocage());
 	}
-	
-	public char getChar(int x, int y){
+
+	public char getChar(int x, int y) {
 		return this.theMap[x][y].Priorite();
 	}
-	
-	public Case getCase(int x, int y){
+
+	public Case getCase(int x, int y) {
 		return this.theMap[x][y];
 	}
-	
-	
-	public int[][] size(){
-		return new int [this.longueur][this.largeur];
+
+	public int[][] size() {
+		return new int[this.longueur][this.largeur];
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		String chaine = "";
-		for(int i=0;i<this.theMap.length;i++){
-			for(int j=0;j<this.theMap[0].length;j++){
+		for (int i = 0; i < this.theMap.length; i++) {
+			for (int j = 0; j < this.theMap[0].length; j++) {
 				chaine += this.theMap[i][j].Priorite();
 			}
 			chaine += "\n";
 		}
 		return chaine;
+	}
+	
+	public void ajouteBombe(int[] c){
+		this.bombes.add(new Bombe(c));
+	}
+	
+	public void incremente(){
+		for(int i=0;i<this.bombes.size();i++){
+			this.bombes.get(i).decounter();
+		}
+		this.explose();
+	}
+	
+	public void explose(){
+		for(int i=0;i<this.bombes.size();i++){
+			//A corriger
+			this.bombes.get(i).explosion;
+		}
 	}
 }
